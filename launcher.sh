c158a2efdb3a775d6e9c20dd107aed006dad9095
Daniele Zanaga <gugout@gmail.com>
2018-02-24 22:17:48 +0100

    modifica launcher

--- launcher.sh
+++ launcher.sh
@@ -2,7 +2,7 @@
 # launcher.sh
 
 cd /home/pi/Flowers/
-sudo -u pi source /home/pi/.venv/jns/bin/activate &
+sudo ./home/pi/.venv/jns/bin/activate &
 sudo -u pi jupyter lab &
 #sudo -u pi python3 pop_db.py &
 #sudo -u pi python3 flowers_bot.py &
