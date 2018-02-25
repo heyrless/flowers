92e6e09633cb879b49d801ebda5fa86496167be8
Daniele Zanaga <gugout@gmail.com>
2018-02-25 15:23:24 +0100

    launcher script

--- .ipynb_checkpoints/launcher-checkpoint.sh
+++ .ipynb_checkpoints/launcher-checkpoint.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 # launcher.sh
 
 cd /home/pi/Flowers/
-sudo ./home/pi/.venv/jns/bin/activate &
+sudo -u pi source /home/pi/.venv/jns/bin/activate &
 sudo -u pi jupyter lab &
 #sudo -u pi python3 pop_db.py &
 #sudo -u pi python3 flowers_bot.py &
