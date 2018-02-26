f12982c227ec2c5a3f5fd5bf5821a74ecf237eb4
Daniele Zanaga <gugout@gmail.com>
2018-02-26 15:04:10 +0100

    l

--- .ipynb_checkpoints/launcher-checkpoint.sh
+++ .ipynb_checkpoints/launcher-checkpoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd /home/pi/flowers
-sudo -u pi source /home/pi/.venv/jns/bin/activate
-sudo -u pi nohup jupyter notebook &>/dev/null &
+source /home/pi/.venv/jns/bin/activate
+nohup jupyter notebook &>/dev/null &
 #sudo -u pi python3 pop_db.py &
 #sudo -u pi python3 flowers_bot.py &
 #sudo -u pi python3 humidity_relay.py &
