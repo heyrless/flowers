3d2eb71771270f47bc20c4d1e8fcf3d7d99ee4d0
Daniele Zanaga <gugout@gmail.com>
2018-02-26 15:01:29 +0100

    launcher

--- .ipynb_checkpoints/launcher-checkpoint.sh
+++ .ipynb_checkpoints/launcher-checkpoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sudo -u pi source /home/pi/flo/bin/activate
+cd /home/pi/flowers
+sudo -u pi source /home/pi/.venv/jns/bin/activate
 sudo -u pi nohup jupyter notebook &>/dev/null &
-sudo -u pi python3 pop_db.py &
-sudo -u pi python3 flowers_bot.py &
-sudo -u pi python3 humidity_relay.py &
-sudo -u pi python3 webcam_timelapse.py &
+#sudo -u pi python3 pop_db.py &
+#sudo -u pi python3 flowers_bot.py &
+#sudo -u pi python3 humidity_relay.py &
+#sudo -u pi python3 webcam_timelapse.py &
