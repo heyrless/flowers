a879a28f90456ef8cf8a5a6ade70b94c4604a1e5
Daniele Zanaga <gugout@gmail.com>
2018-02-26 13:16:35 +0100

    launcher script tuning

--- launcher.sh
+++ launcher.sh
@@ -4,5 +4,5 @@ source /home/pi/flo/bin/activate
 nohup jupyter notebook &>/dev/null &
 python3 pop_db.py &
 python3 flowers_bot.py &
-humidity_relay.py &
+python3 humidity_relay.py &
 python3 webcam_timelapse.py &
